76dac4991992fb9946535839fcf71ae94bb9cd0d
24hr1a0539-code <24hr1a0539@mtieat.org>
2026-01-31 14:29:25 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,14 @@
+public class demo {
+    public static void main(String[] args) {
+        //System.out.println("my name is vani");
+        System.out.print("hello to all");
+        System.out.println("hii");
+        System.out.println("goodafternoon");
+        System.err.println("HII");//
+    int a=25;
+    System.err.println(a);
+        
+    }
+    
+    
+}
