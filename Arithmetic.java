793a804ca6203a0e0db7ee7ab661aac6fa78fd9c
24hr1a0539-code <24hr1a0539@mtieat.org>
2026-02-07 14:07:10 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a=30;
+        int b= 25;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+    
+}
