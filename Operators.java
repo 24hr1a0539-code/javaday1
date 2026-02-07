0f7b54dc78726931cdf2f22392135b42252a7968
24hr1a0539-code <24hr1a0539@mtieat.org>
2026-02-07 15:32:55 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,15 @@
+public class Operators  {
+    public static void main(String[] args) {
+        int a=55;
+        int b=-10;
+        if(a>0)
+        {
+            System.out.println("a is positive");
+        }
+        else
+        {
+         System.out.println(" a is not positive");
+        }
+    }
+ 
+}
