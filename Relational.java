a256b156a584173d394353d56e3125283229f5fc
24hr1a0539-code <24hr1a0539@mtieat.org>
2026-02-07 15:36:35 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,14 @@
+public class Relational {
+    public static void main(String[] args) {
+        float a=20.78f;
+        float b=35.33f;
+    System.out.println(a=b);
+    System.out.println(a<b);
+    System.out.println(a>b);
+    System.out.println(a<=b);
+    System.out.println(a!=b);
+
+    
+
+    }
+}
