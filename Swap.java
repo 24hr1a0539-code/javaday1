1068fa3a4f2897c740ba5f5b8614e3180e6dd354
24hr1a0539-code <24hr1a0539@mtieat.org>
2026-02-07 15:23:43 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,15 @@
+public class Swap {
+    public static void main(String[] args)
+    {
+    int a=20;
+    int b=38;
+    int temp=a;
+    a=b;
+    b=temp;
+    System.out.println(a);
+    System.out.println(b); 
+    System.out.println(temp);
+    }
+}
+
+
