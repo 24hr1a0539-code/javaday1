fc03912d71462f830ca690365546484f267fdf26
24hr1a0539-code <24hr1a0539@mtieat.org>
2026-02-07 10:58:42 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+    public static void main(String[] args){
+
+    
+    int a=6000;
+    byte b=127;
+    short c=233;
+    float d= 10.1f;
+    double e=10.1234567890;
+    boolean f=true;
+    char g='v';
+    
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(g);
+    
+}
+    
+}
